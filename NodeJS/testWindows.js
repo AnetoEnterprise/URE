c95b3380563daa14bd4df7a0383c2a3ba9bb9456
Aneto <Kindlysertir@gmail.com>
2021-12-03 09:37:33 +0100

    Add files via upload

---
+++ NodeJS/testWindows.js
@@ -0,0 +1,5 @@
+const ureModule = require('./Windows/ure.node');
+console.log('URE : ', ureModule.Compilation("-e", "./Demo.txt", ".ure"));
+console.log('URE : ', ureModule.Utilisation("-l", "./Demo.ure", ""));
+//console.log('URE : ', ureModule.Commandes("lister les bases des donnees=`.`;", "./MaBase1.cf"));
+console.log();
